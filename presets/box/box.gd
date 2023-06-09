3a762dc0eeee40406964192956aac726f6078ea4
Afonso Vasconcelos <afonso.vasconcelos@tecnico.ulisboa.pt>
2023-06-09 19:21:55 +0100

    Box basic physics v1.0

--- presets/box/box.gd
+++ presets/box/box.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+extends RigidBody2D
 class_name Box
 
 const SPEED = 350.0
@@ -12,14 +12,12 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-	move_and_slide()
+	pass
 
 func _on_button_button_down():
 	get_tree().change_scene_to_file("res://levels/debug_Level/startmenu.tscn")
-	
-func push(vel: Vector2) -> void:
-	velocity.x = vel.x
-	move_and_slide()
+
+func _integrate_forces(state):
+	angular_velocity = 0
+	rotation_degrees = 0
 
