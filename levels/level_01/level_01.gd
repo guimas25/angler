797da7609d4d1c691eb096cb26fc0228dfa78247
Rodrigo-Rosa <56298893+Rodrigo-Rosa@users.noreply.github.com>
2023-06-19 07:24:53 +0100

    final

--- levels/level_01/level_01.gd
+++ levels/level_01/level_01.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-var enemy_down = false
 var enemy
 var done = false
 
@@ -8,15 +7,18 @@ var done = false
 func _ready():
 	enemy = $Ancient
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if is_instance_valid(enemy):
+		done = false
 	if not is_instance_valid(enemy) and not done:
 		done = true
-		checker()
+		$StaticBody2D.queue_free()
 
 func _on_area_2d_body_entered(body):
+	SaveState.n_deaths = SaveState.n_deaths + 1 
 	get_tree().reload_current_scene()
-	
-func checker():
-	$StaticBody2D.queue_free()
+
+
+func _on_portal_body_entered(body):
+	get_tree().change_scene_to_file("res://levels/level_05/level_05.tscn")
--- levels/level_05/level_05.gd
+++ levels/level_05/level_05.gd
@@ -12,4 +12,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	$AnimatedSprite2D/time.text = "Finish Time: " + str(snapped(SaveState.current_time, 0.01)) + "s"
+	if SaveState.minigame_failure + SaveState.minigame_success != 0:
+		$AnimatedSprite2D/minigame.text = "Minigame rate: " + str(snapped((SaveState.minigame_success/float(SaveState.minigame_failure + SaveState.minigame_success)) * 100.0, 0.01)) + "%"
+	else:
+		$AnimatedSprite2D/minigame.text = "Minigame Success rate: none"
+	$AnimatedSprite2D/n_deaths.text = "nº of deaths: " + str(SaveState.n_deaths)
+	var i = 0
+	for j in SaveState.seen_fish:
+		if j:
+			i = i + 1
+	$AnimatedSprite2D/fishopedia.text = "Fishopedia: " + str(i/8.0 * 100.0) + "%"
+
+
+func _on_area_2d_body_entered(body):
+	SaveState.stop_the_timer()
